3e7943e786d264ab09a665a701c11d1cd252a7ee
Emisael Kisler <ekisler@gmail.com>
2022-11-30 14:55:13 -0400

    conectando front/back

--- src/slices/api.js
+++ src/slices/api.js
@@ -1,4 +1,4 @@
-export const url = "http://localhost:5000/api";
+export const url = "http://https://backend-scanneame-production.up.railway.app/api";
 
 export const setHeaders = () => {
     const headers = {
