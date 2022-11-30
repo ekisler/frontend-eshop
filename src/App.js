f19ea517968cdf8debf11826acc0d565d557265c
Emisael Kisler <ekisler@gmail.com>
2022-11-30 14:07:36 -0400

    first commit

---
+++ src/App.js
@@ -0,0 +1,46 @@
+import "./App.css";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { ToastContainer } from "react-toastify";
+
+import Home from "./components/Home";
+import NavBar from "./components/NavBar";
+import NotFound from "./components/NotFound";
+import Cart from "./components/Cart";
+
+import "react-toastify/dist/ReactToastify.css";
+import Register from "./components/auth/Register";
+import Login from "./components/auth/Login";
+import CheckoutSuccess from "./components/CheckoutSuccess";
+import Dashboard from "./components/admin/Dashboard";
+import Products from "./components/admin/Products";
+import Summary from "./components/admin/Summary";
+import CreateProduct from "./components/admin/CreateProduct";
+
+function App() {
+  return (
+    <div className="App">
+      <BrowserRouter>
+        <ToastContainer />
+        <NavBar />
+        <div className="content-container">
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/cart" element={<Cart />} />
+            <Route path="/checkout-success" element={<CheckoutSuccess />} />
+            <Route path="/register" element={<Register />} />
+            <Route path="/login" element={<Login />} />
+            <Route path="/admin" element={<Dashboard />}>
+              <Route path="products" element={<Products />}>
+                <Route path="create-product" element={<CreateProduct />} />
+              </Route>
+              <Route path="summary" element={<Summary />} />
+            </Route>
+            <Route path="*" element={<NotFound />} />
+          </Routes>
+        </div>
+      </BrowserRouter>
+    </div>
+  );
+}
+
+export default App;
